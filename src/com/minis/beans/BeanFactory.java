7d7d3d4e8e2967bcf3a24bf8b038d6f121711767
pf <pfei.z@foxmail.com>
2023-05-11 16:12:29 +0800

    增加监听以及拓展bean

--- src/com/minis/beans/BeanFactory.java
+++ src/com/minis/beans/BeanFactory.java
@@ -8,5 +8,20 @@ public interface BeanFactory {
      * */
     Object getBean(String beanName) throws BeansException;
     Boolean containsBean(String name);
+
+    /**
+     * registerBean方法在拓展beanfactory接口时取消
+     * @param beanName
+     * @param obj
+     */
     void registerBean(String beanName, Object obj);
+
+    /**
+     * 以下内容为拓展beanfactory接口时新增
+     * @param name
+     * @return
+     */
+    boolean isSingleton(String name); //新增 Singleton 的判断
+    boolean isPrototype(String name); //新增 Prototype 的判断
+    Class<?> getType(String name); //获取 Bean 的类型
 }
