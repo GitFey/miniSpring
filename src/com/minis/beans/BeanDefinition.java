165294db4bafb765f6debd89c03cc2bcc347974c
pf <guohkai@163.com>
2023-05-11 16:12:29 +0800

    增加监听以及拓展bean

--- src/com/minis/beans/BeanDefinition.java
+++ src/com/minis/beans/BeanDefinition.java
@@ -2,8 +2,23 @@ package com.minis.beans;
 
 
 public class BeanDefinition {
+    /**
+     * 以下实例域为bean的最基本的属性
+     */
     private String id;
     private String className;
+    /**
+     * 以下实例域为拓展bean时新增
+     */
+    String SCOPE_SINGLETON = "singleton"; //单例
+    String SCOPE_PROTOTYPE = "prototype"; //原型
+    private String scope = SCOPE_SINGLETON;
+    private boolean lazyInit = false;  //表示 Bean 要不要在加载的时候初始化
+    private String[] dependsOn; //记录 Bean 之间的依赖关系
+    private String initMethodName;  //初始化方法的声明
+    private volatile Object beanClass;
+//    private ArgumentValues constructorArgumentValues; //构造器参数列表
+//    private PropertyValues propertyValues; //构造器属性列表
 
     public BeanDefinition(String id,String className){
         this.id = id;
