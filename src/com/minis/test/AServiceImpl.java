2ded029f921eeaf779a9c8c60020e44c93b1f0fa
pf <pfei.z@foxmail.com>
2023-05-18 17:08:38 +0800

    实现依赖注入

--- src/com/minis/test/AServiceImpl.java
+++ src/com/minis/test/AServiceImpl.java
@@ -2,7 +2,39 @@ package com.minis.test;
 
 
 public class AServiceImpl implements com.minis.test.AService {
-    public void sayHello() {
-        System.out.println("a service 1 say hello");
+    private String name;
+    private int level;
+    private String property1;
+    private String property2;
+
+    public AServiceImpl() {
     }
+    public AServiceImpl(String name, int level) {
+        this.name = name;
+        this.level = level;
+        System.out.println(this.name + "," + this.level);
+    }
+    public void sayHello() {
+        System.out.println(this.property1 + "," + this.property2);
+    }
+
+    public String getProperty1() {
+        return property1;
+    }
+
+    public void setProperty1(String property1) {
+        this.property1 = property1;
+    }
+
+    public String getProperty2() {
+        return property2;
+    }
+
+    public void setProperty2(String property2) {
+        this.property2 = property2;
+    }
+
+//    public void sayHello() {
+//        System.out.println("a service 1 say hello");
+//    }
 }
