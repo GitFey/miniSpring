2f9b410eb783ff46b3d1dffbcd2756802c2f544b
pf <pfei.z@foxmail.com>
2023-05-25 13:43:06 +0800

    实现依赖注入,增加注释

--- src/com/minis/beans/SimpleBeanFactory.java
+++ src/com/minis/beans/SimpleBeanFactory.java
@@ -108,7 +108,10 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
 
 
     /**
-     * creatBean ：1.doCreateBean()  2.handleProperties()
+     * creatBean ：
+     * 创建一个obj，然后 ：
+     *  1.doCreateBean() ： 根据beandefinition ，在ArgumentValues列表中拿到相应的类型和值，返回给obj
+     *  2.handleProperties() ：  根据beandefinition ，在propertyValues列表中拿到相应的类型和值，在obj中增加
      * @param bd
      * @return
      */
@@ -129,6 +132,14 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
         return obj;
     }
 
+    /**
+     * 处理constructor
+     * 首先，获取 XML 配置中的属性值，这个时候它们都是通用的 Object 类型
+     * 我们需要根据 type 字段的定义判断不同 Value 所属的类型，作为一个原始的实现这里我们只提供了 String、Integer 和 int 三种类型的判断。
+     * 最终通过反射构造对象，将配置的属性值注入到了 Bean 对象中，实现构造器注入。
+     * @param bd
+     * @return
+     */
     private Object doCreateBean(BeanDefinition bd) {
         Class<?> clz = null;
         Object obj = null;
@@ -196,6 +207,16 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
 
     }
 
+    /**
+     * 处理property：
+     * 和处理 constructor 相同，我们依然要通过 type 字段确定 Value 的归属类型。
+     * 但不同之处在于，判断好归属类型后，我们还要手动构造 setter 方法，通过反射将属性值注入到 setter 方法之中。通过这种方式来实现对属性的赋值。
+     * 其实代码的核心是通过 Java 的反射机制调用构造器及 setter 方法，在调用过程中根据具体的类型把属性值作为一个参数赋值进去。
+     * 这也是所有的框架在实现 IoC 时的思路。反射技术是 IoC 容器赖以工作的基础。
+     * @param bd
+     * @param clz
+     * @param obj
+     */
     private void handleProperties(BeanDefinition bd, Class<?> clz, Object obj) {
         //handle properties
         System.out.println("handle properties for bean : " + bd.getId());
@@ -209,6 +230,7 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
                 boolean isRef = propertyValue.getIsRef();
                 Class<?>[] paramTypes = new Class<?>[1];
                 Object[] paramValues =   new Object[1];
+                //如果属性不是引用类型，则根据属性的类型设置相应的参数类型，并将属性值赋给参数值数组。
                 if (!isRef) {
                     if ("String".equals(pType) || "java.lang.String".equals(pType)) {
                         paramTypes[0] = String.class;
@@ -225,6 +247,7 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
 
                     paramValues[0] = pValue;
                 }
+                //如果属性是引用类型，则根据属性的类型动态加载相应的类，并通过调用 getBean 方法获取对应的依赖bean，并将其设置为参数值。
                 else { //is ref, create the dependent beans
                     try {
                         paramTypes[0] = Class.forName(pType);
@@ -237,10 +260,11 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
                         e.printStackTrace();
                     }
                 }
-
+                //构造设置属性的方法名，方法名以 "set" 开头，后跟属性名称首字母大写的形式。
                 String methodName = "set" + pName.substring(0,1).toUpperCase() + pName.substring(1);
 
                 Method method = null;
+                //通过反射获取类 clz 中与方法名和参数类型匹配的方法对象。
                 try {
                     method = clz.getMethod(methodName, paramTypes);
                 } catch (NoSuchMethodException e) {
@@ -248,6 +272,7 @@ public class SimpleBeanFactory  extends DefaultSingletonBeanRegistry implements 
                 } catch (SecurityException e) {
                     e.printStackTrace();
                 }
+                //通过反射调用获取到的方法对象，将参数值设置到目标对象 obj 的对应属性中。
                 try {
                     method.invoke(obj, paramValues);
                 } catch (IllegalAccessException e) {
