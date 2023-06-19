bd5c14182af9caa9ed70c23c06630bdd94495f98
pf <pfei.z@foxmail.com>
2023-06-19 14:52:11 +0800

    实现依赖注入,增加注释2

--- src/com/minis/test/Test1.java
+++ src/com/minis/test/Test1.java
@@ -7,8 +7,6 @@ import com.minis.context.ClassPathXmlApplicationContext;
 public class Test1 {
     public static void main(String[] args) throws BeansException {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-//        com.minis.test.AService aService = (com.minis.test.AService)ctx.getBean("aservice");
-//        aService.sayHello();
         AService aService;
         try {
             aService = (AService)ctx.getBean("aservice");
